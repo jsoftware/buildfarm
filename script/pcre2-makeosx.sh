66e0615b5c2e80fc7b12a99a1990878201e706e0
bill lam <bbill.lam@gmail.com>
2024-02-25 14:03:25 +0800

    wip

--- script/pcre2-makeosx.sh
+++ script/pcre2-makeosx.sh
@@ -17,7 +17,6 @@ rm -f src/*.o .libs/*.o
 ./autogen.sh
 
 CC=clang ./configure \
- CPPFLAGS='-arch x86_64 ' \
  --enable-pcre2-8 \
  --disable-pcre2-16 \
  --disable-pcre2-32 \
@@ -32,18 +31,12 @@ cp .libs/libpcre2-8.0.dylib libpcre2-8.0-x86_64.dylib
 
 rm -f src/*.o .libs/*.o
 
-CC=clang ./configure \
- CPPFLAGS='-arch arm64' \
- --enable-pcre2-8 \
- --disable-pcre2-16 \
- --disable-pcre2-32 \
- --disable-debug \
- --disable-jit \
- --enable-bsr-anycrlf \
- --enable-newline-is-anycrlf \
+sed -i "" -e "s/CPPFLAGS= $/CPPFLAGS= -arch arm64/" Makefile
+sed -i "" -e "s/LDFLAGS= $/LDFLAGS= -arch arm64/" Makefile
 
 make -f Makefile clean
 make -f Makefile
 cp .libs/libpcre2-8.0.dylib libpcre2-8.0-arm64.dylib
 
-lipo libpcre2-8.0-x86_64.dylib libpcre2-8.0-arm64.dylib -create -output libpcre2-8.0.dylib 
+rm -f .libs/*.o
+lipo libpcre2-8.0-x86_64.dylib libpcre2-8.0-arm64.dylib -create -output .libs/libpcre2-8.dylib 
