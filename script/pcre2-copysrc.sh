b51ef7ffa427588df5c7989273d59f55b8441233
bill lam <bbill.lam@gmail.com>
2024-02-25 15:57:41 +0800

    wip

--- script/pcre2-copysrc.sh
+++ script/pcre2-copysrc.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
 set -evx
-curl -L -O https://github.com/PCRE2Project/pcre2/archive/master.tar.gz
-tar -xzf master.tar.gz
+if [ "$1" = "linux" ]; then
+  curl -L -O https://github.com/PCRE2Project/pcre2/archive/master.tar.gz
+  tar -xzf master.tar.gz
+elif [ "$1" = "raspberry" ]; then
+  curl -L -O https://github.com/PCRE2Project/pcre2/archive/master.tar.gz
+  tar -xzf master.tar.gz
+elif [ "$1" = "darwin" ]; then
+  curl -L -O https://github.com/PCRE2Project/pcre2/archive/master.tar.gz
+  tar -xzf master.tar.gz
+elif [ "$1" = "android" ]; then
+  curl -L -O https://github.com/PCRE2Project/pcre2/archive/master.tar.gz
+  tar -xzf master.tar.gz
+elif [ "$1" = "openbsd" ]; then
+  curl -L -O https://github.com/PCRE2Project/pcre2/archive/master.zip
+  unzip  master.zip
+elif [ "$1" = "freebsd" ]; then
+  curl -L -O https://github.com/PCRE2Project/pcre2/archive/master.zip
+  unzip master.zip
+elif [ "$1" = "wasm" ]; then
+  curl -L -O https://github.com/PCRE2Project/pcre2/archive/master.tar.gz
+  tar -xzf master.tar.gz
+elif [ "$1" = "win" ]; then
+  curl -L -O https://github.com/PCRE2Project/pcre2/archive/master.tar.gz
+  tar -xzf master.tar.gz
+else
+  echo "argument is linux|darwin|raspberry|android|openbsd|freebsd|wasm|win"
+  exit 1
+fi
+
